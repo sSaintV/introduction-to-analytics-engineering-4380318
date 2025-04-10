e24e904d843792b9c2f283efa4ce27663b08f351
sSaintV <70302136+sSaintV@users.noreply.github.com>
2025-04-10 14:56:31 +0800

    add-in-number-of-states

--- second-challenge/make-pr-here.sql
+++ second-challenge/make-pr-here.sql
@@ -5,8 +5,11 @@ create view product_perf as
         , p.Category as Product_Category
         , p.Price as Product_Price
         , sum(o.Quantity) as Total_Product_Sold
-        , count(o.Order_Id) as Total_Product_Orders
+        , count(o.Order_Id) as Total_Product_Orders   
+        , count(distinct c.state) as Total_States
 
 from products p
 join orders o
     on o.product_id = p.product_id
+join customers c
+    on c.cust_id = o.cust_id
